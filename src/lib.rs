3d8f66a9295e21aaeda3801820cb214a6c3cd476
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-11 15:34:25 +0800

    parse start time from xml

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,21 @@ use std::io::{BufReader, Read};
 mod errors;
 use errors::*;
 
+struct TrackPoint {
+    lat: f64,
+    lon: f64,
+    elevation_meters: f64,
+}
+impl TrackPoint {
+    fn from_xml(elem: xmltree::Element) -> TrackPoint {
+        unimplemented!()
+    }
+}
+
 pub struct Gpx {
-    document: xmltree::Element
+    time: String,
+    track_points: Vec<TrackPoint>,
+    document: xmltree::Element,
 }
 impl Gpx {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Gpx> {
@@ -21,8 +34,27 @@ impl Gpx {
         Self::parse(reader)
     }
     pub fn parse<R: Read>(r: R) -> Result<Gpx> {
+        let document = xmltree::Element::parse(r)?;
         Ok(Gpx {
-            document: xmltree::Element::parse(r)?
+            time: element_get_path(&document, &["metadata", "time"])
+                .unwrap()
+                .text
+                .clone()
+                .unwrap(),
+            track_points: vec![],
+            document: document,
         })
+    }
+}
+
+fn element_get_path<'a>(elem: &'a xmltree::Element, path: &[&str]) -> Option<&'a xmltree::Element> {
+    match path.split_first() {
+        Some((child_name, path)) => {
+            match elem.get_child(*child_name) {
+                Some(child) => element_get_path(child, path),
+                None => None,
+            }
+        }
+        None => Some(elem),
     }
 }
