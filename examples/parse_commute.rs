20466bf3098f42e4a0d4e2e68b5fd7d6a9901321
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-11 16:04:47 +0800

    print number of track points

--- examples/parse_commute.rs
+++ examples/parse_commute.rs
@@ -3,4 +3,5 @@ extern crate strava_gpx;
 fn main() {
     let gpx = strava_gpx::Gpx::open("examples/data/20170517-001923-Ride.gpx").unwrap();
     println!("parsed commute successfully");
+    println!("{} track points", gpx.track_points.len());
 }
